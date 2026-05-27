0d39c17112d05742703039bb37a3c5a61e23290c
Muntyq <aleix.muntal78@gmail.com>
2026-05-27 20:48:40 +0200

    installer fix

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/run/current-system/sw/bin/bash
 
-ssh-keygen github.com >> "$HOME"/.ssh/known_hosts
+ssh-keyscan github.com >> "$HOME"/.ssh/known_hosts
 git --git-dir="$HOME"/.dotfiles --work-tree="$HOME" config --local status.showUntrackedFiles no
 
 # Following line rm's original dotfiles, change for commented lines to keep them as backup
