c089b40febd86c37e085c3e169ddf0b9a567307c
Muntyq <aleix.muntal78@gmail.com>
2026-06-02 20:53:02 +0200

    added new_nvim

---
+++ new_nvim/core/options.lua
@@ -0,0 +1,56 @@
+local opt = vim.opt
+
+-- Ui ------------------
+opt.number = true
+opt.relativenumber = true
+opt.signcolumn = "yes"
+opt.cursorline = true
+opt.inccomand = "split"
+opt.termguicolors = true
+opt.showmode = true
+opt.cmdheight = 1
+
+-- Text behaviour ------
+opt.scrolloff = 10
+opt.sidescrolloff = 10
+opt.wrap = true
+opt.tabstop = 5
+opt.shiftwidth = 5
+opt.softtabstop = 5
+opt.expandtab = true
+opt.smartindent = true
+opt.breakindent = true
+opt.autoindent = true
+opt.list = true
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Tmux ----------------
+opt.splitright = true
+opt.splitbelow = true
+
+-- Files ---------------
+opt.undofile = true
+opt.swapfile = false
+opt.backup = false
+opt.updatetime = 250
+opt.timeoutlen = 300
+
+-- Search --------------
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = false
+opt.incsearch = true
+
+-- Folding (treesitter)-
+-- opt.foldmethod = "expr"
+-- opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+-- opt.foldlevel = 99            -- open all folds by default
+
+-- Misc ----------------
+opt.mouse = "a"
+opt.pumheight = 10 --popup menu heigh
+opt.completeopt = "menuone,noinsert,noselect" --auto complete things if using nvim-cmp probably use menu,menuone
+opt.shortmess:append("c") --search info, its about ignoring info
+opt.confirm = true
+
+vim.schedule(function() opt.clipboard = "unnamedplus" end)
