13195b0f1d3f198877f5b6364a811b53f0fdfbd0
Muntyq <aleix.muntal78@gmail.com>
2026-06-08 07:39:46 +0200

    fixed transparency

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -138,8 +138,8 @@ hl.config({
 		blur             = {
 			enabled  = true,
 			size     = 3,
-			passes   = 1,
-			vibrancy = 0.1696,
+			passes   = 2,
+			vibrancy = 0.2,
 		},
 	},
 
--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -1,6 +1,19 @@
 vim.pack.add({ { src = 'https://github.com/rebelot/kanagawa.nvim' }, })
 vim.cmd.colorscheme('kanagawa-wave')
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+-- Rm background alien thingy
+local function clear_bg(group)
+	local hl = vim.api.nvim_get_hl(0, { name = group, link = false })
+	hl.bg = nil
+	hl.ctermbg = nil
+	vim.api.nvim_set_hl(0, group, hl)
+end
+
+local groups = {
+	"Normal", "NormalNC", "NormalFloat",
+	"SignColumn", "EndOfBuffer",
+}
+
+for _, g in ipairs(groups) do
+	clear_bg(g)
+end
