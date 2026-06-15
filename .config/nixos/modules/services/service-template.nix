382f5511d7e59dba89bf207e5c5a43261c0fd584
Muntyq <aleix.muntal78@gmail.com>
2026-06-15 08:08:31 +0200

    added websockets to template

--- .config/nixos/modules/services/service-template.nix
+++ .config/nixos/modules/services/service-template.nix
@@ -15,24 +15,24 @@ in {
     # youre app
 
     # local; blocky + nginx
-    # services.blocky.settings.customDNS.mapping."${subdomain}${domain}" = "${localIP}";
-    #
-    # services.nginx.virtualHosts = {
-    #
-    #     "status.${domain}" = {
-    #         useACMEHost = "${domain}";
-    #         forceSSL = true;
-    #         locations."/" = {
-    #           proxyPass = "http://127.0.0.1:${port}";
-    #           proxyWebsockets = true; # when needed
-    #       };
-    #     };
-    # };
+    services.blocky.settings.customDNS.mapping."${subdomain}${domain}" = "${localIP}";
+    services.nginx.virtualHosts = {
+        "status.${domain}" = {
+            useACMEHost = "${domain}";
+            forceSSL = true;
+            locations."/" = {
+              proxyPass = "http://127.0.0.1:${port}";
+              # proxyWebsockets = true; # if needed
+          };
+        };
+    };
 
     # public; cloudflared + nginx
-    # services.cloudflared.tunnels."${tunnel}".ingress."${subdomain}.${domain}" = "http://localhost:80";
-    # services.nginx.virtualHosts."${subdomain}.${domain}" = {
-    #     useACMEHost = "${domain}";
-    #     locations."/".proxyPass = "http://127.0.0.1:${port}";
-    # };
+    services.cloudflared.tunnels."${tunnel}".ingress."${subdomain}.${domain}" = "http://localhost:80";
+    services.nginx.virtualHosts."${subdomain}.${domain}" = {
+        useACMEHost = "${domain}";
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:${port}";
+          # proxyWebsockets = true; # if needed
+    };
 }
